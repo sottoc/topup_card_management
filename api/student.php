65a734ab1ba725be265f39b76c95d629c57b1370
Travis DeveloperTra <you@example.com>
2018-11-23 02:38:33 -0800

    update student api

--- api/student.php
+++ api/student.php
@@ -8,8 +8,19 @@
         
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
+                $family_code = $row['Family_code'];
+                $query1 = "SELECT amount FROM `tbl_family_code_amount` WHERE `family_code`='".$family_code."'";
+                $result1 = $conn->query($query1);
+                $amount = 0;
+                if ($result1->num_rows > 0) {
+                    while($row1 = $result1->fetch_assoc()) {
+                        $amount = $row1['amount'];
+                    }
+                }
+                $row['Card_Value'] = $amount;
                 display_results($row, 'OK');
             }
+
         } else{
             display_results("No result", "ERROR");
         }
@@ -23,8 +34,19 @@
         
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
+                $family_code = $row['Family_code'];
+                $query1 = "SELECT amount FROM `tbl_family_code_amount` WHERE `family_code`='".$family_code."'";
+                $result1 = $conn->query($query1);
+                $amount = 0;
+                if ($result1->num_rows > 0) {
+                    while($row1 = $result1->fetch_assoc()) {
+                        $amount = $row1['amount'];
+                    }
+                }
+                $row['Card_Value'] = $amount;
                 display_results($row, 'OK');
             }
+
         } else{
             display_results("No result", "ERROR");
         }
--- topup_paypal.php
+++ topup_paypal.php
@@ -61,48 +61,39 @@
 		<br/>
 
 		<form action="<?php echo $paypal_url; ?>" method="post" name="frmPayPal1">
-		<input type="hidden" name="business" value="<?php echo $paypal_id; ?>">
-		<input type="hidden" name="cmd" value="_xclick">
-		
-		<!-- <div class="frm">
-			<div class="frm_label">Student Name :</div>
-			<input type="text" name="item_name">
-		</div>
-		<div class="frm">
-			<div class="frm_label">Student Id :</div>
-			<input type="text" name="item_number">
-		</div> -->
-		<div class="frm" style="display:none;">
-			<div class="frm_label">Amount : </div>
-			<input type="text" name="amount">
-		</div>
-		
-		<!--div class="frm">
-			<div class="frm_label">Invoice : </div>
-			<input type="text" name="invoice" value="">
-		</div-->
-		
-		<!--div class="frm">
-			<div class="frm_label">Item name : </div>
-			 <input type="text" name="item_name" value="PHPGang Payment">
-		</div-->
+			<input type="hidden" name="business" value="<?php echo $paypal_id; ?>">
+			<input type="hidden" name="cmd" value="_xclick">
+			
+			<!-- <input type="text" name="item_name" value="item name"> -->
+			<!-- <div class="frm">
+				<div class="frm_label">Student Name :</div>
+				<input type="text" name="item_name">
+			</div>
+			<div class="frm">
+				<div class="frm_label">Student Id :</div>
+				<input type="text" name="item_number">
+			</div> -->
+			<div class="frm" style="display:none;">
+				<div class="frm_label">Amount : </div>
+				<input type="text" name="amount">
+			</div>
+			
+			<input type="hidden" name="no_shipping" value="2">
 	
-		<input type="hidden" name="no_shipping" value="1">
-  
-		<input type="hidden" name="no_note" value="0" /> 
-		<input type="hidden" name="rm" value="2">
-		<input type="hidden" name="handling" value="0">
-		<!--input type="hidden" name="cpp_header_image" value="http://localhost/topup_card_management_dec5/images/topup_logotext.gif"-->
-		<input type="hidden" name="currency_code" value="SGD">
-		<!--input type="hidden" name="handling" value="0"-->
-		<input type="hidden" name="cancel_return" value="<?php echo $rootpath; ?>/cancel.php">
-		<input type="hidden" name="return" value="<?php echo $rootpath; ?>/success.php">
-		<div>
-			<input type="submit" name="btnsubmit" value="Top-up Now" class="btn control-button" style="height:100% !important;">
-		</div>
-		
-		<!--input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"-->
-		<!--img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"-->
+			<input type="hidden" name="no_note" value="0" /> 
+			<input type="hidden" name="rm" value="2">
+			<input type="hidden" name="handling" value="0">
+			<!--input type="hidden" name="cpp_header_image" value="http://localhost/topup_card_management_dec5/images/topup_logotext.gif"-->
+			<input type="hidden" name="currency_code" value="SGD">
+			<!--input type="hidden" name="handling" value="0"-->
+			<input type="hidden" name="cancel_return" value="<?php echo $rootpath; ?>/cancel.php">
+			<input type="hidden" name="return" value="<?php echo $rootpath; ?>/success.php">
+			<div>
+				<input type="submit" name="btnsubmit" value="Top-up Now" class="btn control-button" style="height:100% !important;">
+			</div>
+			
+			<!--input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"-->
+			<!--img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"-->
 		</form> 
 		
 	</div>
